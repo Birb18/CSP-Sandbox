3548d30f2942e59898a3df454e62cfc843b7d276
Birb18 <raccevan@gmail.com>
2025-11-07 22:17:23 +0000

    lockaddiply

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,9 @@
 def main() -> None:
     num=int(input())
     for _ in range(num):
-        print(input())
+        x, y=input().split(" ")
+        x=int(x)
+        y=int(y)
+        print(f'{x+y} {x*y}')
 if __name__ == "__main__":
     main()
